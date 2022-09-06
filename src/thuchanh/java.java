3041c5c89d3ce6f0a10890498a105fa74726870f
sangpk <dinhsangcnc@gmail.com>
2022-09-06 09:13:41 +0700

    thuchanh

---
+++ src/thuchanh/java.java
@@ -0,0 +1,10 @@
+package thuchanh;
+
+import java.util.Date;
+
+public class java {
+    public static void main(String[] args) {
+       Date now=new Date();
+        System.out.println("now is: "+ now);
+    }
+}
